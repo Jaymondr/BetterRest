55f3dbec5c9b2c39ba47eb8e83d6e939f8dccdef
Jaymond Richardson <jaymond@givego.com>
2023-06-17 09:26:16 -0600

    Adds project files and updates file directory

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jaymond Richardson on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Jaymond Richardson on 6/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
